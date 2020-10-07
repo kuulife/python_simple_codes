93f0ee30f393a3a6e4d4571a98ef8b0f602be9fb
a.amangeldiev <a.amanangeldiev@beaver-development.com>
2020-10-07 12:56:25 +0600

    cylinder

---
+++ cylinder/main.py
@@ -0,0 +1,16 @@
+class Cylinder:
+    pi = 3.14
+    def __init__(self,height=1,radius=1):
+       self.height = height
+       self.radius = radius
+
+        
+    def volume(self):
+        return self.pi*((self.radius)**2)*self.height
+    
+    def surface_area(self):
+        return (2*self.pi*self.radius)*(self.radius + self.height)
+
+c = Cylinder(2,3)
+print(f'volume of cylinder is: {c.volume()}')
+print(f'surface are of cylinder is: {c.surface_area()}')
