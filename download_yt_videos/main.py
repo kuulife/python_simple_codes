94919eb5e928def5d6d1a5035facbec509019ffc
a.amangeldiev <a.amanangeldiev@beaver-development.com>
2020-10-08 21:42:50 +0600

    program for download youtube videos

---
+++ download_yt_videos/main.py
@@ -0,0 +1,19 @@
+import pafy #importing pafy
+import youtube_dl
+#getting url of yoiutube video to be downloaded
+url = 'https://youtu.be/7gLXKYfZPYc'
+video = pafy.new(url)
+
+#getting all available streams
+streams = video.streams
+
+#printing all available streams
+for i  in streams:
+	print(i)
+
+#get best resolution ragardless of format
+best = video.getbest()
+
+print(best.resolution, best.extension)
+best.download()
+print('video downloaded successfully')
