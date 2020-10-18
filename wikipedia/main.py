6445fc46660d0624a59c544669fd683b3a008ea7
a.amangeldiev <kuu.life@gmail.com>
2020-10-18 22:31:28 +0600

    wikipedia search

---
+++ wikipedia/main.py
@@ -0,0 +1,15 @@
+
+# import wikipedia
+
+# print(wikipedia.summary("Wikipedia"))
+
+# print(wikipedia.search("Barack"))
+
+# ny = wikipedia.page("New York")
+# # ny.title
+# ny.url
+# ny.content
+# ny.links[0]
+
+# wikipedia.set_lang("fr")
+# wikipedia.summary("Facebook", sentences=1)
