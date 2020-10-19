0b8a79a110f06985c784007256974169004dd7c8
a.amangeldiev <kuu.life@gmail.com>
2020-10-19 11:05:53 +0600

    track covid

--- kyrgyz_covid_tracker/main.py
+++ kyrgyz_covid_tracker/main.py
@@ -3,5 +3,20 @@ from bs4 import BeautifulSoup
 
 res = requests.get('https://covid.kg/ru')
 soup = BeautifulSoup(res.text, 'lxml')
-covid = soup.select('.data-number.data-today')[0].text.strip()
-print('today Covid Disease cases :',covid+' in Kyrgyzstan')
+
+data_recovered = soup.select('.data-recovered')[0].text.strip()
+covid_cases = soup.select('.data-number.data-today')[0].text.strip()
+data_name = soup.select('.data-name')[0].text.strip()
+data_info = soup.select('.data-info')[0].text.strip()
+data_title = soup.select('.data-title')[0].text.strip()
+data_title_recovered = soup.select('.data-title')[1].text.strip()
+data_death = soup.select('.data-death')[0].text.strip()
+data_title_death = soup.select('.data-title')[2].text.strip()
+
+print(data_name)
+print(data_info+'\n')
+
+print(f'{data_title}: {covid_cases}')
+print(f'{data_title_recovered}: {data_recovered}')
+print(f'{data_title_death}: {data_death}')
+print('\n')
